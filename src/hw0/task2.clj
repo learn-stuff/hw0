64fee6d65456dd0f6064a32bbff40754d69c2bed
Andrey Bakuta <anb@anahoret.com>
2014-05-23 19:07:55 +0300

    Done task 2

--- src/hw0/task2.clj
+++ src/hw0/task2.clj
@@ -1,11 +1,19 @@
 (ns hw0.task2)
 
+(defn- binary-numeric? [s]
+  (if-let [s (seq s)]
+    (let [s (if (= (first s) \-) (next s) s)
+          s (drop-while #(or (= % \1) (= % \0)) s)]
+      (empty? s))))
+
+
 (defn hello
   "Takes a string representing a name
   and returns the string \"Hello, \"
   concatenated with the name."
   [name]
   (str "Hello, " name))
+
 
 (defn starts-with-vowel?
   "Takes a string
@@ -17,3 +25,14 @@
   (let [vowels #{\a \e \i \o \u}
         s (clojure.string/lower-case s)]
     (contains? vowels (first s))))
+
+
+(defn binary-multiple-of-4?
+  "Takes a string
+  and returns true if the string represents a binary number
+  that is a multiple of 4.
+  NOTE: be sure it returns false if the string is not a valid binary number!"
+  [s]
+  (and
+    (binary-numeric? s)
+    (= 0 (mod (Integer/parseInt s 2) 4))))
--- test/hw0/task2_test.clj
+++ test/hw0/task2_test.clj
@@ -20,3 +20,20 @@
     (not (starts-with-vowel? ""))
     (not (starts-with-vowel? "123aaa"))
     (not (starts-with-vowel? "!aaa"))))
+
+
+(deftest binary-multiple-of-4
+  (testing "Test binary multiple of 4 for empty string"
+    (not (binary-multiple-of-4? "")))
+
+  (testing "Test binary multiple of 4 for non alpha string"
+    (not (binary-multiple-of-4? "Batman")))
+
+  (testing "Test binary multiple of 4 for non binaries"
+    (not (binary-multiple-of-4? "11112")))
+
+  (testing "Test binary multiple of 4 for binaries non multiple of 4"
+    (not (binary-multiple-of-4? "11111")))
+
+  (testing "Test binary multiple of 4 for binaries non multiple of 4"
+    (is (binary-multiple-of-4? "10000"))))
